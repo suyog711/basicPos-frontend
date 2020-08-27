e3f007743ae4623913cd0c734a4f3ef6c33b48eb
suyog <suyogpradhan711@gmail.com>
2020-08-27 09:43:27 +0545

    admin lte initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import Sidebar from './components/sidebar';
+import Footer from './components/footer';
+import Dashboard from './components/dashboard';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <div>
+          <Header />
+          <Sidebar />
+          <Route path="/dashboard" component={Dashboard} />
+          <Footer />
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/dashboard/dashboard.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import Dashboard from './dashboard';
+
+describe('Dashboard', () => {
+  test('matches snapshot', () => {
+    const wrapper = shallow(<Dashboard />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
